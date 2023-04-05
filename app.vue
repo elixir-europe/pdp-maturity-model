b6c0174dfb124b3301831db6a531ec6663e3cf44
Imane MESSAK <messak.imane@gmail.com>
2023-04-05 10:32:32 +0200

    Custumize Sass variables

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
-<template>
+<!-- <template>
   <div>
-    <NuxtWelcome />
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+  </div>
+  <template>
+  <v-btn>Hello Sass Changes</v-btn>
+</template>
+
+</template> -->
+
+
+ 
+
+<template>
+  <div class="d-flex align-center flex-column">
+    <v-icon>mdi-home</v-icon>
+    <v-card width="400">
+      <v-card-title>This is a title</v-card-title>
+      <v-card-subtitle>This is a subtitle</v-card-subtitle>
+      <v-card-text> This is content </v-card-text>
+    </v-card>
+    <v-btn class="mt-10">Hello Sass Changes</v-btn>
   </div>
 </template>
+
+<style lang="scss">
+@use './settings';
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+import { createResolver } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass'],
+    css: ['vuetify/lib/styles/main.sass',
+          '@mdi/font/css/materialdesignicons.min.css',],
     build: {
         transpile: ['vuetify'],
+    },
+    hooks: {
+        'vite:extendConfig': (config) => {
+          config.plugins.push(
+            vuetify({
+              styles: { configFile: resolve('./settings.scss') },
+            })
+          )
+        }
     },
     vite: {
         define: {
