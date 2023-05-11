c841ba3df956d2bc48c1b4ba080e63e0cc5a2f6a
Thomas Denecker <thomas.denecker@gmail.com>
2023-05-11 11:41:57 +0200

    update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -11,6 +11,14 @@ git add -A
 git commit -m 'deploy'
 git push -f git@github.com:elixir-europe/fosp-maturity-model.git main:main
 
-touch .output/public/.nojekyll
 
-push-dir --dir=dist --branch=gh-pages --cleanup
+# navigate into the build output directory
+touch .output/public/.nojekyll 
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:elixir-europe/fosp-maturity-model.git main:gh-pages
+cd ..
